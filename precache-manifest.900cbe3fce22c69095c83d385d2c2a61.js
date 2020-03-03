c7334706db712147ee2998604617ea098d944a2f
Jakub <jakubsmuszko@gmail.com>
2020-03-03 13:46:26 +0100

    Updates

---
+++ precache-manifest.900cbe3fce22c69095c83d385d2c2a61.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f1be7975a93d9e77650bb293a9564d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f179645a68abeb5755c1",
+    "url": "/portfolio/static/css/main.a14ca0d7.chunk.css"
+  },
+  {
+    "revision": "bcaa39b69f9e3eecfae8",
+    "url": "/portfolio/static/js/2.2f626b6a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.2f626b6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f179645a68abeb5755c1",
+    "url": "/portfolio/static/js/main.46498b3a.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/portfolio/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/portfolio/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css-logo.3707da3c.svg"
+  },
+  {
+    "revision": "f66c069e5a8b437c14be8b9105e0cf16",
+    "url": "/portfolio/static/media/doglify-cover.f66c069e.PNG"
+  },
+  {
+    "revision": "1e000dbebc93f874159bf430517702ea",
+    "url": "/portfolio/static/media/fotocv.1e000dbe.jpg"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/portfolio/static/media/html-logo.f2282f94.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/portfolio/static/media/node-logo.270f55ff.png"
+  },
+  {
+    "revision": "c15f588af723baaa9a7733f3b3f24e83",
+    "url": "/portfolio/static/media/pizzeria-cover.c15f588a.PNG"
+  },
+  {
+    "revision": "9a3650611b77fdb6d005f3196f29eb20",
+    "url": "/portfolio/static/media/portfolio-cover.9a365061.PNG"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react-logo.47ce6e77.png"
+  }
+]);
